167dc2bcc85539f126c901a6d6fc49e851b5b3f1
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-10 10:30:33 -0600

    Move post and make a successful post from app, update state to trigger re-render with new post

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import {getOrders} from '../../apiCalls';
+import { postOrder } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
@@ -21,12 +22,29 @@ class App extends Component {
       .catch(err => console.error('Error fetching:', err));
   }
 
+
+  addOrder = (newOrder) => {
+    postOrder("http://localhost:3001/api/v1/orders", newOrder)
+      .then(result => {
+        if (result.id) {
+          this.setState({ orders: [...this.state.orders, result], error: '' })
+        } else {
+          this.setState({ error: 'Please send a valid order!' })
+        }
+      }).catch(err =>  console.error("This is your error from post", err))
+  }
+
+  // postOrder("http://localhost:3001/api/v1/orders", data)
+  //     .then(res => console.log("This is your response from post", res))
+  //     .catch(err =>  console.error("This is your error from post", err))
+
+
   render() {
     return (
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
-          <OrderForm />
+          <OrderForm addOrder={this.addOrder} />
         </header>
 
         <Orders orders={this.state.orders}/>
