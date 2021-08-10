ccfda039364c4bf4c65b452043b33d505515b73f
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-10 11:16:15 -0600

    Finish testing stubs and network

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,5 +1,6 @@
 describe('Main app page', () => {
     beforeEach(() => {
+        
         cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
         statusCode: 200,
         fixture: 'orders.json'
@@ -10,5 +11,25 @@ describe('Main app page', () => {
     it('Should contain header with zenbox text' , ()=> {
         cy.contains('h1', 'Burrito Builder');
     })
-    
-})
+
+    it('Should be able to show a new post', () => {
+        cy.visit('http://localhost:3000/')
+        cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
+        statusCode: 201,
+        body: {
+          name: "Leta",
+          ingredients: ['sofritas']
+        }
+      })
+            cy.get('input').type('Leta')
+            cy.get('[name="sofritas"]').click();
+            cy.get(':nth-child(16)').click()
+
+            cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
+            statusCode: 200,
+            fixture: 'orders2.json'
+            });
+            cy.visit('http://localhost:3000');
+            
+    });
+});
