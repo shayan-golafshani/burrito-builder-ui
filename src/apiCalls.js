ca42dbf5b0c621239b5300af340c664ab7331d78
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-10 10:16:37 -0600

    Add post using mdn docs

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export async function postOrder(url, data) {
+  const response = await fetch(url, {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json'  
+    },
+    body: JSON.stringify(data)
+  });
+  return response.json();
+}
