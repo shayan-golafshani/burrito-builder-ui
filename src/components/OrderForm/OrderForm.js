a7a85208e9a7081e6cd01500c3558adcc44a23e8
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-10 09:55:42 -0600

    Handle ingredient change, prevent duplicate ingredients in order, and add clear order button

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -10,6 +10,22 @@ class OrderForm extends Component {
     };
   }
 
+  handleIngredientChange = e => {
+    e.preventDefault();
+
+    console.log(e.target.name, "THIS IS THE CLICK EVENT, I NEED TO GET THE VALUE AND UPDATE THE INGREDIENTS LIST")
+
+    if(!this.state.ingredients.includes(e.target.name)) {
+      this.setState({
+        ingredients: [...this.state.ingredients, e.target.name]
+      }, () => {
+        console.log(this.state.ingredients);
+      });
+    } else {
+      console.log("SORRY YOU'VE ALREADY ADDED THAT INGREDIENT")
+    }
+
+  }
 
   handleNameChange = e => {
     e.preventDefault()
@@ -53,6 +69,13 @@ class OrderForm extends Component {
         />
 
         { ingredientButtons }
+
+        <button onClick={
+          e => {
+            e.preventDefault()
+            this.setState({ingredients:[]})
+          }
+        }>Clear Order</button>
 
         <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
 
